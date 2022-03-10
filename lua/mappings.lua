fba64be951eeb6cf12d9eeb36961cde7d2afeb3d
ge3224 <ge3224@gmail.com>
2022-03-10 12:19:22 -0600

    Add Telescope buffer mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,48 +7,62 @@ local options = { noremap = true }
 vim.g.mapleader = ' ' -- 'vim.g' sets global variables
 
 -- Edit init.lua
-map('n', '<leader>ev', ':vsplit $MYVIMRC<CR>', options)
+vim.keymap.set('n', '<leader>ev', ':vsplit $MYVIMRC<CR>', options)
 
 -- Terminal escape
-map('t', '<esc>', ':<C-\\><C-n>', options)
+vim.keymap.set('t', '<esc>', ':<C-\\><C-n>', options)
 
 -- Source init.lua
-map('n', '<leader>sv', ':luafile $MYVIMRC<CR>', options)
+vim.keymap.set('n', '<leader>sv', ':luafile $MYVIMRC<CR>', options)
 
 -- Telescope mappings
-map('n', '<leader>ff', [[<Cmd>Telescope find_files<CR>]], options)
-map('n', '<leader>fg', [[<Cmd>Telescope live_grep<CR>]], options)
-map('n', '<leader>fh', [[<Cmd>Telescope buffers<CR>]], options)
+vim.keymap.set("n", "<leader>ff", [[<Cmd>Telescope find_files<CR>]], options)
+vim.keymap.set("n", "<leader>fg", [[<Cmd>Telescope live_grep<CR>]], options)
+vim.keymap.set("n", "<leader>fh", [[<Cmd>Telescope buffers<CR>]], options)
+vim.keymap.set("n", "<leader>fb", "<Cmd>Telescope buffers<CR>", options)
 
 -- harpoon
-map('n', '<leader>H', [[:lua require("harpoon.mark").add_file()<CR>]], options)
-map('n', '<leader>u', [[:lua require("harpoon.ui").nav_file(1)<CR>]], options)
-map('n', '<leader>i', [[:lua require("harpoon.ui").nav_file(2)<CR>]], options)
-map('n', '<leader>o', [[:lua require("harpoon.ui").nav_file(3)<CR>]], options)
-map('n', '<leader>p', [[:lua require("harpoon.ui").nav_file(4)<CR>]], options)
-map('n', '<leader>y', [[:lua require("harpoon.ui").toggle_quick_menu()<CR>]], options)
+vim.keymap.set('n', '<leader>H', [[:lua require("harpoon.mark").add_file()<CR>]], options)
+vim.keymap.set('n', '<leader>u', [[:lua require("harpoon.ui").nav_file(1)<CR>]], options)
+vim.keymap.set('n', '<leader>i', [[:lua require("harpoon.ui").nav_file(2)<CR>]], options)
+vim.keymap.set('n', '<leader>o', [[:lua require("harpoon.ui").nav_file(3)<CR>]], options)
+vim.keymap.set('n', '<leader>p', [[:lua require("harpoon.ui").nav_file(4)<CR>]], options)
+vim.keymap.set('n', '<leader>y', [[:lua require("harpoon.ui").toggle_quick_menu()<CR>]], options)
 
 
 -- Moving through split windows
-map('n', 'gj', [[<C-w>j]], options)
-map('n', 'gh', [[<C-w>h]], options)
-map('n', 'gk', [[<C-w>k]], options)
-map('n', 'gl', [[<C-w>l]], options)
+vim.keymap.set('n', 'gj', [[<C-w>j]], options)
+vim.keymap.set('n', 'gh', [[<C-w>h]], options)
+vim.keymap.set('n', 'gk', [[<C-w>k]], options)
+vim.keymap.set('n', 'gl', [[<C-w>l]], options)
 
 -- Remove duplicates
-map('n', '<leader>rd', [[mz:g/^\(.*\)\n\1$/d<CR>'z]], options)
+vim.keymap.set('n', '<leader>rd', [[mz:g/^\(.*\)\n\1$/d<CR>'z]], options)
 
 -- Remove extra whitespace
-map('n', '<leader>ws', [[vip:s/\s\{2,}/ /g<CR>]], options)
+vim.keymap.set('n', '<leader>ws', [[vip:s/\s\{2,}/ /g<CR>]], options)
 
 -- Yank the full file path of the current buffer
-map('n', '<leader>fp', [[:let @+ = expand("%:p")<CR>]], options)
+vim.keymap.set('n', '<leader>fp', [[:let @+ = expand("%:p")<CR>]], options)
 
 -- Extend css utility classes
-map('n', '<leader>bs', [[:s/^\s\{0,}/\t@extend ./:s/\(\w\)\s\(\w\)/\1, .\2/g:s/\(\w\)\s\{0,}$/\1;]], options)
+vim.keymap.set('n', '<leader>bs', [[:s/^\s\{0,}/\t@extend ./:s/\(\w\)\s\(\w\)/\1, .\2/g:s/\(\w\)\s\{0,}$/\1;]], options)
 
 -- Open calendar in split view
 vim.keymap.set("n", "<leader>cv", "<cmd>Calendar -view=year -split=vertical -width=27<CR>", options)
 
 -- Open quickfix list
 vim.keymap.set("n", "<leader>qf", "<cmd>copen<CR><c-w>J", options);
+
+
+-- source mappings.lua
+local srcmd = "nvim/lua/mappings.lua<CR>"
+local os = vim.loop.os_uname().sysname
+
+if os == "Linux" then
+  srcmd = "<cmd>luafile ~/.config/" .. srcmd
+elseif os == "Windows_NT" then
+  srcmd = "<cmd>luafile $LOCALAPPDATA/" .. srcmd
+end
+
+vim.keymap.set("n", "<leader>sm", srcmd)
