a119369b1a952b41f2fbb725ba832af5bf260d50
ge3224 <ge3224@gmail.com>
2022-03-13 14:18:17 -0500

    Refactor snippet parsing

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -32,7 +32,6 @@ if os == "Linux" then
 elseif os == "Windows_NT" then
   srcmd = "<cmd>luafile $LOCALAPPDATA/" .. srcmd
 end
-
 vim.keymap.set("n", "<leader>ss", srcmd)
 
 local es = {
@@ -40,31 +39,38 @@ local es = {
 }
 
 local html = {
-    -- html boilerplate
-    doc_type = {'html', '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset="utf-8">\n\t\t<title>$1</title>\n\t</head>\n\t<body>\n\t</body>\n</html>'},
 
-    -- a tag
-    anchor = {'a', '<a class="$1" href="$2">$0</a>'},
+  element = { 'e', '<$1 class"$2">$3</$0>' },
 
-    -- div tags with class
-    div = {'d', '<div class="$1">\n\t$0\n</div>'},
+  -- html boilerplate
+  doc_type = {'html', '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset="utf-8">\n\t\t<title>$1</title>\n\t</head>\n\t<body>\n\t</body>\n</html>'},
 
-    -- div start only
-    div_start = {'ds', '<div class="$0">'},
+  -- a tag
+  anchor = {'a', '<a class="$1" href="$2">$0</a>'},
 
-    -- img tag with class
-    img = {'img', '<img src="$1" class="$0">'},
+  -- div tags with class
+  div = {'d', '<div class="$1">\n\t$0\n</div>'},
 
-    -- srcset block
-    srcset = {'srcset', '<picture class="$1">\n\t<source media="(min-width:1200px)" srcset="$2">\n\t<source media="(min-width:992px)" srcset="$3">\n\t<source media="(min-width:768px)" srcset="$4">\n\t<source media="(min-width:576px)" srcset="$5">\n\t<img src="$6" alt="$0">\n</picture>'},
+  -- div start only
+  div_start = {'ds', '<div class="$0">'},
 
-    -- https://github.com/yuin/goldmark
-    goldmark = {'md', '{{ call . "$0" }}'},
+  -- img tag with class
+  img = {'img', '<img src="$1" class="$0">'},
+
+  -- srcset block
+  srcset = {'srcset', '<picture class="$1">\n\t<source media="(min-width:1200px)" srcset="$2">\n\t<source media="(min-width:992px)" srcset="$3">\n\t<source media="(min-width:768px)" srcset="$4">\n\t<source media="(min-width:576px)" srcset="$5">\n\t<img src="$6" alt="$0">\n</picture>'},
+
+  -- https://github.com/yuin/goldmark
+  goldmark = {'md', '{{ call . "$0" }}'},
 }
 
 local css = {
-    -- basic selector
-    class_selector = {'.', '.$1 {\n\t$0\n}'},
+    -- basic selectors
+    class_selector = {'c', '.$1 {\n\t$0\n}'},
+
+    element_selector = {'e', '$1 {\n\t$0\n}'},
+
+    id_selector = {'i', '#$1 {\n\t$0\n}'},
 
     -- media queries
     media_query = {'m', '@media screen and (min-width: $1) {\n\t$0\n}'},
@@ -73,44 +79,26 @@ local css = {
     grid = {'mf', '.$0 {\n\tdisplay: block;\n\n\t@media screen and (min-width: vars.\\$sm) {\n\t\tbackground: pink;\n\t}\n\n\t@media screen and (min-width: vars.\\$md) {\n\t\tbackground: lightblue;\n\t}\n\n\t@media screen and (min-width: vars.\\$lg) {\n\t\tbackground: lightgreen;\n\t}\n\n\t@media screen and (min-width: vars.\\$xl) {\n\t\tbackground: orange;\n\t}\n}'},
 }
 
-
-ls.snippets = {
-  all = {
-  },
-  typescript = {
-    ls.parser.parse_snippet(es.loop[1], es.loop[2]),
-  },
-  javascript = {
-    ls.parser.parse_snippet(es.loop[1], es.loop[2]),
-  },
-  scss = {
-    ls.parser.parse_snippet(css.class_selector[1], css.class_selector[2]),
-    ls.parser.parse_snippet(css.media_query[1], css.class_selector[2]),
-    ls.parser.parse_snippet(css.grid[1], css.grid[2]),
-  },
-  css = {
-    ls.parser.parse_snippet(css.class_selector[1], css.class_selector[2]),
-    ls.parser.parse_snippet(css.media_query[1], css.class_selector[2]),
-    ls.parser.parse_snippet(css.grid[1], css.grid[2]),
-  },
-  html = {
-    ls.parser.parse_snippet(html.div[1], html.div[2]),
-    ls.parser.parse_snippet(html.div_start[1], html.div_start[2]),
-    ls.parser.parse_snippet(html.doc_type[1], html.doc_type[2]),
-    ls.parser.parse_snippet(html.img[1], html.img[2]),
-    ls.parser.parse_snippet(html.srcset[1], html.srcset[2]),
-    ls.parser.parse_snippet(html.goldmark[1], html.goldmark[2]),
-  },
-  svelte = {
-    ls.parser.parse_snippet(es.loop[1], es.loop[2]),
-    ls.parser.parse_snippet(html.div[1], html.div[2]),
-    ls.parser.parse_snippet(html.div_start[1], html.div_start[2]),
-    ls.parser.parse_snippet(html.doc_type[1], html.doc_type[2]),
-    ls.parser.parse_snippet(html.img[1], html.img[2]),
-    ls.parser.parse_snippet(html.srcset[1], html.srcset[2]),
-    ls.parser.parse_snippet(html.goldmark[1], html.goldmark[2]),
-  },
-  lua = {
-    ls.parser.parse_snippet('mc', '--[[\n$0\n]]--'),
-  },
+local lua = {
+  block_comments = {'mc', '--[[\n$0\n]]--'}
 }
+
+local function parse_snippet_collection(filetype, collection)
+  if ls.snippets[filetype] == nil then
+    ls.snippets[filetype] = {}
+  end
+
+  local f = ls.snippets[filetype]
+  for _, snippet in pairs(collection) do
+    table.insert(f, ls.parser.parse_snippet(snippet[1], snippet[2]))
+  end
+end
+
+parse_snippet_collection("html", html)
+parse_snippet_collection("javascript", es)
+parse_snippet_collection("typescript", es)
+parse_snippet_collection("svelte", es)
+parse_snippet_collection("svelte", html)
+parse_snippet_collection("css", css)
+parse_snippet_collection("scss", css)
+parse_snippet_collection("lua", lua)
