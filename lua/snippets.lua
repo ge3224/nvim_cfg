00229f4a70b74872099815bb4a261957afa12a76
ge3224 <ge3224@gmail.com>
2022-05-01 15:36:47 -0500

    Update luasnip cfg

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -1,4 +1,18 @@
-local ls = require "luasnip"
+local ls = require"luasnip"
+local s = ls.snippet
+-- local sn = ls.snippet_node
+-- local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local events = require("luasnip.util.events")
+-- local ai = require("luasnip.nodes.absolute_indexer")
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local m = require("luasnip.extras").m
+-- local lambda = require("luasnip.extras").l
 
 ls.config.set_config {
   history = true,
@@ -34,71 +48,84 @@ elseif os == "Windows_NT" then
 end
 vim.keymap.set("n", "<leader>ss", srcmd)
 
-local es = {
-  loop = { 'f', 'for (let i = 0; i < $1.length; i++) {\n\t$0\n}'},
-}
+ls.add_snippets("scss", {
+  s("c", {
+    t({"."}), i(1, "class_name"), t(" {"),
+    t({"","\t"}), i(2, "declaration"),
+    t({"", "}"})
+  }),
+  s("m", {
+    t({"@media screen and (min-width: "}), i(1, "breakpoint"), t({") {"}),
+    t({"","\t"}), i(2, "declaration"),
+    t({"", "}"})
+  }),
+  s("x", {
+    t("@extend ."), i(1, "class_name"), t(";")
+  })
+})
 
-local html = {
+ls.filetype_extend("css", { "scss" })
 
-  element = { 'e', '<$1 class"$2">$3</$0>' },
+ls.add_snippets("javascript", {
+  s("f", {
+    t("for (let i = 0; i, "), i(1, "<array>"), t(".length; i++) {"),
+    t({"", "\t"}), i(2, "<code block>"),
+    t({"", "}"}),
+  }),
+  s("n", {
+    t("if ("), i(1, "<object>"), t(" !== null && "), i(2, "<object>"), t(" !== undefined && "), i(3, "<object>"), t(" !== void 0) {"),
+    t({"", "\t"}), i(4, "<block>"),
+    t({"", "}"})
+  }),
+  s("nc", {
+    t("if ("), i(1, "<object>"), t(" === null || "), i(2, "<object>"), t(" === undefined || "), i(3, "<object>"), t(" === void 0) {"),
+    t({"", "\t"}), i(4, "<block>"),
+    t({"", "}"})
+  })
+})
 
-  -- html boilerplate
-  doc_type = {'html', '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset="utf-8">\n\t\t<title>$1</title>\n\t</head>\n\t<body>\n\t</body>\n</html>'},
+ls.filetype_extend("svelte", { "scss", "html", "javascript" });
 
-  -- a tag
-  anchor = {'a', '<a class="$1" href="$2">$0</a>'},
+ls.add_snippets("all", {
+  s("doc", {
+    t({"<!DOCTYPE html>", "<html>"}),
+    t({"", "\t<head>"}),
+    t({"", "\t\t<meta charset=\"utf-8\">"}),
+    t({"", "\t\t<title>"}), i(1, "text..."), t({"</title>"}),
+    t({"", "\t</head>"}),
+    t({"", "\t<body>"}),
+    t({"", "\t\t\t"}), i(2, "continue..."),
+    t({"", "\t</body>"}),
+    t({"", "</html>"})
+  }),
+  s("be", {
+    t({"<"}), i(1, "tag..."), t({">"}),
+    t({"", "\t"}), i(2, "content..."),
+    t({"", "</"}), i(3, "tag..."), t({">"})
+  }),
+  s("ie", {
+    t({"<"}), i(1, "tag..."), t({">"}), i(2, "content..."), t({"</"}), i(3, "tag..."), t({">"})
+  })
+})
 
-  -- div tags with class
-  div = {'d', '<div class="$1">\n\t$0\n</div>'},
+ls.add_snippets("lua", {
+  s("t", {
+    t("t({\""), i(1, "text..."), t("\"})")
+  }),
+  s("i", {
+    t({"i("}), i(1, "number..."), t({", \""}), i(2, "text..."), t({"\")"})
+  }),
+  s("s", {
+    t({"s(\""}), i(1, "trigger..."), t({"\", {"}),
+    t({"", "\t"}), i(2, "code_block..."),
+    t({"", "})"})
+  })
+})
 
-  -- div start only
-  div_start = {'ds', '<div class="$0">'},
-
-  -- img tag with class
-  img = {'img', '<img src="$1" class="$0">'},
-
-  -- srcset block
-  srcset = {'srcset', '<picture class="$1">\n\t<source media="(min-width:1200px)" srcset="$2">\n\t<source media="(min-width:992px)" srcset="$3">\n\t<source media="(min-width:768px)" srcset="$4">\n\t<source media="(min-width:576px)" srcset="$5">\n\t<img src="$6" alt="$0">\n</picture>'},
-
-  -- https://github.com/yuin/goldmark
-  goldmark = {'md', '{{ call . "$0" }}'},
-}
-
-local css = {
-    -- basic selectors
-    class_selector = {'c', '.$1 {\n\t$0\n}'},
-
-    element_selector = {'e', '$1 {\n\t$0\n}'},
-
-    id_selector = {'i', '#$1 {\n\t$0\n}'},
-
-    -- media queries
-    media_query = {'m', '@media screen and (min-width: $1) {\n\t$0\n}'},
-
-    -- setting up a default grid for a component
-    grid = {'mf', '.$0 {\n\tdisplay: block;\n\n\t@media screen and (min-width: vars.\\$sm) {\n\t\tbackground: pink;\n\t}\n\n\t@media screen and (min-width: vars.\\$md) {\n\t\tbackground: lightblue;\n\t}\n\n\t@media screen and (min-width: vars.\\$lg) {\n\t\tbackground: lightgreen;\n\t}\n\n\t@media screen and (min-width: vars.\\$xl) {\n\t\tbackground: orange;\n\t}\n}'},
-}
-
-local lua = {
-  block_comments = {'mc', '--[[\n$0\n]]--'}
-}
-
-local function parse_snippet_collection(filetype, collection)
-  if ls.snippets[filetype] == nil then
-    ls.snippets[filetype] = {}
-  end
-
-  local f = ls.snippets[filetype]
-  for _, snippet in pairs(collection) do
-    table.insert(f, ls.parser.parse_snippet(snippet[1], snippet[2]))
-  end
-end
-
-parse_snippet_collection("html", html)
-parse_snippet_collection("javascript", es)
-parse_snippet_collection("typescript", es)
-parse_snippet_collection("svelte", es)
-parse_snippet_collection("svelte", html)
-parse_snippet_collection("css", css)
-parse_snippet_collection("scss", css)
-parse_snippet_collection("lua", lua)
+ls.add_snippets("go", {
+  s("e", {
+    t({"if err != nil {"}),
+    t({"", "\t"}), i(1, "block..."),
+    t({"", "}"})
+  })
+})
