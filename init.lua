cd6a9012b892523d2fdc2895d6778fa8b460ee0b
ge3224 <ge3224@gmail.com>
2023-07-29 14:57:00 -0500

    Merge kickstart.nvim bug fix 734cc4e

--- init.lua
+++ init.lua
@@ -449,6 +449,7 @@ mason_lspconfig.setup_handlers {
       capabilities = capabilities,
       on_attach = on_attach,
       settings = servers[server_name],
+      filetypes = (servers[server_name] or {}).filetypes,
     }
   end,
 }
